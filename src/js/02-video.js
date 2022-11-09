6b7a94762370034da3a2eae11b31d47e6fcba3ab
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-09 17:07:18 +0200

    'update'

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import {
+   saveToLS,
+   loadFromLS,
+   removeLS
+ } from './storage.js';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+function loadData() {
+   for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage[LOCALSTORAGE_KEY]) {
+         const ObjectLS = loadFromLS(LOCALSTORAGE_KEY);
+         player.setCurrentTime(ObjectLS['seconds']);
+      }
+   }
+ }
+ loadData();
 
 const onPlay = function(data) {
-   // data is an object containing properties specific to that event
-   return data.timeupdate();
+   console.log('played the video!');
+   console.log(data);
+   saveToLS(LOCALSTORAGE_KEY, data);
 };
 
-player.on('play', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -24,16 +24,14 @@ function loadData() {
  loadData();
 
 form.addEventListener('input', throttle(onMsgElemInput, 500));
+
 function onMsgElemInput(e) {
-   console.log(e.target);
-   console.log(e.target.name);
    formData[e.target.name] = e.target.value;
-   console.log(formData);
    saveToLS(LOCALSTORAGE_KEY, formData);
 }
 
 form.addEventListener('submit', e => {
-   e.preventDefault();
+   e.preventDefault(); 
    removeLS(LOCALSTORAGE_KEY);
    form.reset();
    console.log(formData);
